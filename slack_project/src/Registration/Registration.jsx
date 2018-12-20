0fa79572fc7df0dbfaf419b16b01a0b3e984f191
Pavel Karpenko <ospkiev@gmail.com>
2018-12-20 14:28:21 +0200

    registration_form

--- slack_project/src/Registration/Registration.jsx
+++ slack_project/src/Registration/Registration.jsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
 
 class Registration extends Component {
     render() {
         return (
-            <div>
-                Registration
-            </div>
+            <Grid textAlign='center' verticalAlign='middle' className='app'>
+                <Grid.Column style={{ maxWidth: 450 }}>
+
+                    <Header textAlign='center' icon color='orange' as='h2'>
+                        <Icon name='commet alternate' color='orange' />
+                        Register For Slack Replic
+                </Header>
+                    <Form size='large'>
+                        <Segment stacked>
+                            <Form.Input fluid name='username' icon='user' iconPosition='left' placeholder='Username' type='text' />
+                            <Form.Input fluid name='email' icon='mail' iconPosition='left' placeholder='Email' type='email' />
+                            <Form.Input fluid name='password' icon='lock' iconPosition='left' placeholder='Password' type='password' />
+                            <Form.Input fluid name='passwordConfirm' icon='repeat' iconPosition='left' placeholder='Password Confirm' type='password' />
+                            <Button color='grey' fluid size='large'>Submit</Button>
+
+                        </Segment>
+
+                    </Form>
+                    <Message>
+                        Already a user?
+                    <NavLink to='/Login'>Login</NavLink>
+                    </Message>
+                </Grid.Column>
+            </Grid>
         );
     }
 }
