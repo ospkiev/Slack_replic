3427a47f719608a004136279ecf9fc2655a48b49
Pavel Karpenko <ospkiev@gmail.com>
2018-12-21 15:45:56 +0200

    channels

--- slack_project/src/MarkUp/sidePanel/SidePanel.jsx
+++ slack_project/src/MarkUp/sidePanel/SidePanel.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
 import UserPanel from '../UserPAnel/UserPanel';
+import Channels from '../../Channels/Channels';
 
 class SidePanel extends Component {
     render() {
         return (
             <Menu size='large' inverted fixed='left' vertical style={{ background: '#4c3c4c', fontSize: '1.2rem' }}>
                 <UserPanel />
+                <Channels />
             </Menu>
         );
     }
