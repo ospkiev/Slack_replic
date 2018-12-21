60da8ed5f98279be2b3eae109697d7b1ffa02cee
Pavel Karpenko <ospkiev@gmail.com>
2018-12-21 10:49:28 +0200

    form_login

--- slack_project/src/Login/Login.jsx
+++ slack_project/src/Login/Login.jsx
@@ -10,9 +10,8 @@ class Login extends Component {
         username: '',
         email: '',
         password: '',
-        passwordConfirm: '',
         errors: [],
-        userRef: firebase.database().ref('users')
+        // userRef: firebase.database().ref('users')
     }
 
     handleChange = (e) => {
@@ -23,8 +22,8 @@ class Login extends Component {
         })
     }
 
-    isFormEmpty = ({ username, email, password, passwordConfirm }) => {
-        if (username.length > 0 && email.length > 0 && password.length > 0 && passwordConfirm.length > 0) {
+    isFormEmpty = ({ email, password }) => {
+        if (email.length > 0 && password.length > 0) {
             return true;
         } else {
             return false;
@@ -35,17 +34,9 @@ class Login extends Component {
         let errors = [];
         let error;
 
-        if (!this.isFormEmpty(this.state)) {
+        if (this.isFormEmpty(this.state)) {
             error = {
                 message: 'Fill all fields'
-            };
-            this.setState({
-                errors: errors.concat(error)
-            })
-            return false;
-        } else if (!this.isPasswordValid(this.state)) {
-            error = {
-                message: 'Password is invalid'
             };
             this.setState({
                 errors: errors.concat(error)
@@ -61,32 +52,61 @@ class Login extends Component {
         return errors.some(el => el.message.toLowerCase().includes(inputName)) ? 'error' : '';
     }
 
-    render() {
-        return (
-            <div>
-                <Grid textAlign='center' verticalAlign='middle' className='app'>
-                    <Grid.Column style={{ maxWidth: 450 }}>
+    hadleSubmit = e => {
+        e.preventDefault();
+        if (this.isFormValid(this.state)) {
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(this.state.email, this.state.password)
+                .then(signedInUser => {
+                    console.log(signedInUser);
+                })
+                .catch(err => {
+                    console.log(err);
+                    this.setState({
+                        errors: this.state.errors.concat(err),
+                    })
+                })
+        }
+    }
 
-                        <Header textAlign='center' icon color='blue' as='h2'>
-                            <Icon name='code branch' color='blue' />
-                            Login to Slack Replic
+    render() {
+
+        const { errors, password, email } = this.state
+        return (
+
+            <Grid textAlign='center' verticalAlign='middle' className='app'>
+                <Grid.Column style={{ maxWidth: 450 }}>
+
+                    <Header textAlign='center' icon color='blue' as='h2'>
+                        <Icon name='code branch' color='blue' />
+                        Login to Slack Replic
                         </Header>
-                        <Form size='large' onSubmit={this.handleSubmit}>
-                            <Segment stacked>
-                                <Form.Input className fluid name='username' icon='mail' iconPosition='left' placeholder='Email' type='text' onChange={this.handleChange} />
-                                <Form.Input className fluid name='password' icon='lock' iconPosition='left' placeholder='Password' type='password' onChange={this.handleChange} />
-                                <Button color='grey' fluid size='large'>Login</Button>
-                            </Segment>
-                        </Form>
-                        <Message>
-                            Don`t have an account?
-                    <NavLink to='/registration'>Registration</NavLink>
+                    <Form size='large' onSubmit={this.hadleSubmit}>
+                        <Segment stacked>
+                            <Form.Input className={this.handleInput(errors, 'email')} fluid name='username' icon='mail' iconPosition='left' placeholder='Email' type='text' onChange={this.handleChange} />
+                            <Form.Input className={this.handleInput(errors, 'password')} fluid name='password' icon='lock' iconPosition='left' placeholder='Password' type='password' onChange={this.handleChange} />
+                            <Button color='grey' fluid size='large'>Login</Button>
+                        </Segment>
+                    </Form>
+
+                    {errors.length > 0 && (
+                        <Message error>
+                            <h3>Error</h3>
+                            {errors.map(el => <p key={el.message}>{el.message}</p>)}
                         </Message>
-                    </Grid.Column>
-                </Grid>
-            </div>
+                    )}
+                    <Message>
+                        Don`t have an account?
+                    <NavLink to='/registration'>Registration</NavLink>
+                    </Message>
+                </Grid.Column>
+            </Grid>
+
         );
     }
 }
+
+
 
 export default Login;
