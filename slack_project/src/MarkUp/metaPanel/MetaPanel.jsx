7433ffcc11ae18dc9b4ca47b66dce352e2bc6d02
Pavel Karpenko <ospkiev@gmail.com>
2018-12-26 11:47:47 +0200

    meta_panel

--- slack_project/src/MarkUp/metaPanel/MetaPanel.jsx
+++ slack_project/src/MarkUp/metaPanel/MetaPanel.jsx
@@ -1,13 +1,71 @@
 import React, { Component } from 'react';
+import { Segment, Accordion, Header, Icon, Image } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
 class MetaPanel extends Component {
+    state = {
+        activeIndex: 0,
+    }
+
+    setActiveIndex = (event, titleProps) => {
+        const { index } = titleProps;
+        const { activeIndex } = this.state;
+        const newIndex = activeIndex === index ? -1 : index;
+        this.setState({ activeIndex: newIndex });
+    }
+
+
     render() {
+        console.log(this.props.channel);
+        const { activeIndex } = this.state;
+        if (this.props.channel) {
+            if (this.props.channel.isPrivatChannel) {
+                return null;
+            }
+        }
         return (
-            <div>
-                MetaPanel
-            </div>
+            <Segment>
+                <Header as='h3' attached='top'>
+                    {this.props.channel && this.props.channel.name}
+                </Header>
+                <Accordion styled attached='top'>
+                <Accordion.Title
+                active={activeIndex === 0} index={0} onClick={this.setActiveIndex}>
+                <Icon name='dropdown'/>
+                <Icon name='info'/>
+                Channel Details     
+                </Accordion.Title>
+
+                <Accordion.Content active={activeIndex === 0}>
+                {this.props.channel && this.props.channel.details}
+                </Accordion.Content>
+                <Accordion.Title active={activeIndex === 1} index={1} onClick={this.setActiveIndex}>
+                <Icon name='dropdown'/>
+                <Icon name='pencil alternate'/>
+                Created By
+                
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 1}>
+                <Header as='h3'>
+                
+                <Image circular scr={this.props.channel && this.props.channel.createdBy.photoURL}/>  /// need fix
+                {this.props.channel && this.props.channel.createdBy.name}
+                {/* {this.props.channel && this.props.channel.createdBy.photoURL} */}
+                
+                </Header>
+                </Accordion.Content>
+                </Accordion>
+            </Segment>
+
         );
     }
 }
 
-export default MetaPanel;
+function mapStateToProps(state){
+    return{
+        channel: state.channel,
+        user: state.user.currentUser,
+    }
+}
+
+export default connect(mapStateToProps,null)(MetaPanel);
